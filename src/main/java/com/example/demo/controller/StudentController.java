795b6baf90b2a2ac389a860854dd5f8d58bebc2d
bala-amypo <bala@amypo.in>
2025-12-18 05:19:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.StudentEntity;
+import com.example.demo.service.StudentService;
+
+@RestController
+public class StudentController {
+    @Autowired
+    StudentService st;
+
+    @PostMapping("/addstudent")
+    public StudentEntity addstudent(@RequestBody StudentEntity student){
+        return st.addstudent(student);
+    }
+    
+    @GetMapping("/getallstudents")
+    public List<StudentEntity> getallstudents(){
+        return st.getallstudents();
+    }
+}
+
+
---
+++ src/main/java/com/example/demo/model/StudentModel.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class StudentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "roll_no", nullable = false, unique = true)
+    private String rollNo;
+    private String name;
+    private String department;
+    private int year;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRollNo() {
+        return rollNo;
+    }
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public StudentModel(Long id, String rollNo, String name, String department, int year) {
+        this.id = id;
+        this.rollNo = rollNo;
+        this.name = name;
+        this.department = department;
+        this.year = year;
+    }
+    public StudentModel() {
+    } 
+}
