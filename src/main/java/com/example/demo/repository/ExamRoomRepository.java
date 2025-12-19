392c6bc21c0ebc1e0d73f9df1d6e1676274081b8
bala-amypo <bala@amypo.in>
2025-12-19 10:21:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.ExamRoom;
-
-public interface ExamRoomRepository extends JpaRepository<ExamRoom , Long> {
-    
-}
