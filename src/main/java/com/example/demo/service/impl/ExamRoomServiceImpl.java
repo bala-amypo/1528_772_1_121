47f451d49de56c38752e15978861cc4a0569a13b
bala-amypo <bala@amypo.in>
2025-12-21 17:07:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ExamRoomServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamRoomServiceImpl.java
@@ -1,9 +1,9 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.exception.ApiException;
 import com.example.demo.model.ExamRoom;
 import com.example.demo.repository.ExamRoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.ExamRoomService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +11,11 @@ import java.util.List;
 @Service
 public class ExamRoomServiceImpl implements ExamRoomService {
 
-    @Autowired
-    private ExamRoomRepository examRoomRepository;
+    private final ExamRoomRepository examRoomRepository;
+
+    public ExamRoomServiceImpl(ExamRoomRepository examRoomRepository) {
+        this.examRoomRepository = examRoomRepository;
+    }
 
     @Override
     public ExamRoom addRoom(ExamRoom room) {
--- src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
@@ -1,10 +1,10 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.exception.ApiException;
 import com.example.demo.model.ExamSession;
 import com.example.demo.repository.ExamSessionRepository;
 import com.example.demo.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.ExamSessionService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -12,11 +12,14 @@ import java.time.LocalDate;
 @Service
 public class ExamSessionServiceImpl implements ExamSessionService {
 
-    @Autowired
-    private ExamSessionRepository examSessionRepository;
+    private final ExamSessionRepository examSessionRepository;
+    private final StudentRepository studentRepository;
 
-    @Autowired
-    private StudentRepository studentRepository;
+    public ExamSessionServiceImpl(ExamSessionRepository examSessionRepository,
+                                  StudentRepository studentRepository) {
+        this.examSessionRepository = examSessionRepository;
+        this.studentRepository = studentRepository;
+    }
 
     @Override
     public ExamSession createSession(ExamSession session) {
