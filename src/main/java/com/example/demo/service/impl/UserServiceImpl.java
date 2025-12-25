de9be539d2a553ba2239549d4cf2412a46d4c6d6
bala-amypo <bala@amypo.in>
2025-12-25 14:02:24 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -2,17 +2,21 @@ package com.example.demo.service.impl;
 
 import com.example.demo.exception.ApiException;
 import com.example.demo.model.User;
-import com.example.demo.service.UserService;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository){
+    public UserServiceImpl(UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -26,7 +30,8 @@ public class UserServiceImpl implements UserService {
             user.setRole("STAFF");
         }
 
-        // password already handled as per your working setup
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
 
